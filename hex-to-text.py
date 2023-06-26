23a775670f3b8aa40901e8625693de59d4225f98
itsallah0ax <137829237+itsallah0ax@users.noreply.github.com>
2023-06-26 16:05:51 -0400

    Add files via upload

---
+++ hex-to-text.py
@@ -0,0 +1,19 @@
+import argparse
+import re
+
+# Create the argument parser
+parser = argparse.ArgumentParser(description='Hex to human-readable converter')
+parser.add_argument('-i', '--input', type=str, help='Input file containing the hex string')
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+# Read the hex string from the input file
+with open(args.input, 'r') as file:
+    hex_string = file.read().strip()
+
+# Convert hex string to bytes and remove non-printable ASCII characters
+decoded_string = bytes.fromhex(hex_string).decode('latin')
+decoded_string = re.sub(r'[^\x20-\x7E]', '', decoded_string)
+
+print(decoded_string)
